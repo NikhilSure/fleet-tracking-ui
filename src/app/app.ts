1bff5425b3cd84c4ac3c2f36a6f6da3ae9880f82
nikhil.sure <surenikhil10@gmail.com>
2026-05-22 00:30:51 +0530

    m Integration of API alert and cdr

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { KpiCard } from './shared/components/kpi-card/kpi-card';
 import { Analytics } from "./layouts/analytics/analytics";
 import { LiveMapPage } from "./layouts/live-map-page/live-map-page";
+import { HttpClient } from '@angular/common/http';
+import { PaginatorModule } from 'primeng/paginator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, KpiCard, Analytics, LiveMapPage],
+  imports: [KpiCard, Analytics, LiveMapPage, PaginatorModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -35,47 +38,191 @@ export class App {
     }
   ]
 
-    // ALERT FEED DATA
-  alerts: any = [
-    {
-      id: 1,
-      title: 'Overspeed Alert',
-      message: 'TN09AB1234 exceeded speed limit (102 km/h)',
-      time: '2m ago',
-      type: 'danger',
-      icon: 'pi pi-exclamation-triangle'
-    },
-    {
-      id: 2,
-      title: 'Geofence Exit',
-      message: 'Truck exited Hyderabad zone boundary',
-      time: '5m ago',
-      type: 'warning',
-      icon: 'pi pi-map-marker'
-    },
-    {
-      id: 3,
-      title: 'Vehicle Idle',
-      message: 'Vehicle idle for more than 30 minutes',
-      time: '10m ago',
-      type: 'info',
-      icon: 'pi pi-pause-circle'
-    },
-    {
-      id: 4,
-      title: 'Route Deviation',
-      message: 'Vehicle deviated from planned route',
-      time: '15m ago',
-      type: 'warning',
-      icon: 'pi pi-directions'
-    },
-    {
-      id: 5,
-      title: 'Engine On Alert',
-      message: 'Vehicle started after long stop',
-      time: '20m ago',
-      type: 'info',
-      icon: 'pi pi-power-off'
+  alerts: any[] = [];
+  allAlerts: any[] = [];
+
+  pagination: any = {
+    totalRecords: 0,
+    rows: 5,
+    first: 0,
+    loading: false
+  };
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.getAlerts();
+  }
+
+  getAlerts(page: number = 0, size: number = 5): void {
+    this.pagination.loading = true;
+
+    this.http
+      .get<any>( `http://localhost:8070/api/alerts?page=${page}&size=${size}`)
+      .subscribe({
+        next: (res) => {
+          let response = res.success ? res.data : [];
+        this.alerts = (response?.content || []).map((alert: any) => ({
+
+                id: alert.id,
+
+                title: this.getAlertTitle(alert.alert_type),
+
+                message:
+                  alert.message ||
+                  `${alert.vehicleLocation?.vehicleId} alert triggered`,
+
+                time: this.getTimeAgo(
+                  alert.vehicleLocation?.timestamp || alert.timestamp
+                ),
+
+                type: this.getSeverityType(alert.severity),
+
+                icon: this.getAlertIcon(alert.alert_type),
+
+                severity: alert.severity,
+
+                vehicleId: alert.vehicleLocation?.vehicleId,
+
+                lat: alert.vehicleLocation?.lat,
+
+                lng: alert.vehicleLocation?.lng,
+
+                speed: alert.vehicleLocation?.speed,
+
+                fuelLevel: alert.vehicleLocation?.fuelLevel,
+
+                engineStatus: alert.vehicleLocation?.engineStatus
+              }));
+
+        this.pagination.totalRecords =
+          response.totalElements;
+
+        this.pagination.rows =
+          response.size;
+
+        this.pagination.first =
+          response.page * response.size;
+
+          console.log('Loaded alerts', this.alerts)
+        this.pagination.loading = false;
+        this.cdr.detectChanges();
+        },
+
+        error: (err) => {
+          console.error('Failed to load alerts', err);
+          this.pagination.loading = false;
+        }
+      });
+  }
+
+  onPageChange(event: any): void {
+
+    this.pagination.first = event.first;
+
+    this.pagination.rows = event.rows;
+
+    const page =
+      event.first / event.rows;
+
+    this.getAlerts(
+      page,
+      event.rows
+    );
+  }
+
+  getAlertTitle(type: string): string {
+    switch (type) {
+
+      case 'OVER_SPEED':
+        return 'Overspeed Alert';
+
+      case 'ENGINE_OFF':
+        return 'Engine Off Alert';
+
+      case 'ENGINE_ON':
+        return 'Engine On Alert';
+
+      case 'GEOFENCE_EXIT':
+        return 'Geofence Exit';
+
+      case 'ROUTE_DEVIATION':
+        return 'Route Deviation';
+
+      case 'VEHICLE_IDLE':
+        return 'Vehicle Idle';
+
+      default:
+        return 'Vehicle Alert';
     }
-  ];
+  }
+
+  getSeverityType(severity: string): string {
+    switch (severity) {
+
+      case 'HIGH':
+        return 'danger';
+
+      case 'MEDIUM':
+        return 'warning';
+
+      case 'LOW':
+        return 'info';
+
+      default:
+        return 'info';
+    }
+  }
+
+  getAlertIcon(type: string): string {
+    switch (type) {
+
+      case 'OVER_SPEED':
+        return 'pi pi-exclamation-triangle';
+
+      case 'ENGINE_OFF':
+      case 'ENGINE_ON':
+        return 'pi pi-power-off';
+
+      case 'GEOFENCE_EXIT':
+        return 'pi pi-map-marker';
+
+      case 'ROUTE_DEVIATION':
+        return 'pi pi-directions';
+
+      case 'VEHICLE_IDLE':
+        return 'pi pi-pause-circle';
+
+      default:
+        return 'pi pi-bell';
+    }
+  }
+
+  getTimeAgo(timestamp: number): string {
+
+    if (!timestamp) {
+      return 'Just now';
+    }
+
+    const seconds = Math.floor((Date.now() - timestamp) / 1000);
+
+    const intervals: any = {
+      year: 31536000,
+      month: 2592000,
+      day: 86400,
+      hour: 3600,
+      minute: 60
+    };
+
+    for (const key in intervals) {
+
+      const value = Math.floor(seconds / intervals[key]);
+
+      if (value > 0) {
+        return `${value}${key.charAt(0)} ago`;
+      }
+    }
+
+    return 'Just now';
+  }
 }
